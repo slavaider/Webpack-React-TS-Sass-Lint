5dc1f2505cca136dc1150127b884e89c8b90918b
slavaider <slavaider1@gmail.com>
2021-10-10 19:13:36 +0500

    refactor: delete console.log

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,6 @@ ReactDOM.render(
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 if (module.hot) {
-  console.log("hot");
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   module.hot.accept();
